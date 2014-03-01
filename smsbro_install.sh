28d01c9516be9af68c02d5ad23c04ee382775931
Herpiko Dwi Aguno <herpiko@gmail.com>
2014-03-01 16:10:12 +0800

    add database import script

--- smsbro_install.sh
+++ smsbro_install.sh
@@ -18,7 +18,8 @@ pwd=${pwd}
 clear
 cd /tmp/
 rm -rf master*
-url='http://localhost/download/master.tar.gz'
+url='https://github.com/herpiko/smsbro/archive/master.tar.gz'
+#url='http://localhost/download/master.tar.gz'
 echo -n "Sedang mengunduh. Mohon menunggu...    "
 echo -n "    "
     wget --progress=dot $url 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
@@ -56,24 +57,24 @@ echo -n "MySQL user : "
 read user
 echo -n "MySQL password : "
 read passwd
-echo ""
-echo "Anda perlu mengisi informasi di bawah ini untuk database gammu-smsd (smsd)."
-echo "Anda boleh tidak mengisinya (kosong) jika smsd terletak pada MySQL server yang sama dengan database SMSBro"
-echo -n "SMSD's MySQL host : "
-read smsdhost
-if [[ -z "$smsdhost" ]]; then
-	smsdhost="$host"
-fi
-echo -n "SMSD's MySQL user : "
-read smsduser
-if [[ -z "$smsduser" ]]; then
-	smsduser="$user"
-fi
-echo -n "SMSD's MySQL password : "
-read smsdpasswd
-if [[ -z "$smsdpasswd" ]]; then
-	smsdpasswd="$passwd"
-fi
+# echo ""
+# echo "Anda perlu mengisi informasi di bawah ini untuk database gammu-smsd (smsd)."
+# echo "Anda boleh tidak mengisinya (kosong) jika smsd terletak pada MySQL server yang sama dengan database SMSBro"
+# echo -n "SMSD's MySQL host : "
+# read smsdhost
+# if [[ -z "$smsdhost" ]]; then
+# 	smsdhost="$host"
+# fi
+# echo -n "SMSD's MySQL user : "
+# read smsduser
+# if [[ -z "$smsduser" ]]; then
+# 	smsduser="$user"
+# fi
+# echo -n "SMSD's MySQL password : "
+# read smsdpasswd
+# if [[ -z "$smsdpasswd" ]]; then
+# 	smsdpasswd="$passwd"
+# fi
 
 echo ""
 echo "Tekan tombol enter untuk melanjutkan..."
@@ -85,12 +86,28 @@ rm -rf /tmp/smsbro-master/
 
 cd $fullpath
 sed -i -e 's/php\/smsbro/'$path'/g' application/config/config.php
+sed -i -e 's/localhost/'$host'/g' application/config/database.php
+sed -i -e 's/root/'$user'/g' application/config/database.php
+sed -i -e 's/1/'$passwd'/g' application/config/database.php
 mv htaccess .htaccess
 sed -i -e 's/php\/smsbro/'$path'/g' .htaccess
 mkdir -p /opt/smsbro
 cp -vR $fullpath/opt-bin /opt/smsbro/
 
-mysql -u root --password="$pass" -h $host smsbro < $fullpath/db/smsbro.sql
+result=`mysql -u $user -p$passwd --skip-column-names -e "SHOW DATABASES LIKE 'smsbro'"`
+if [ -z "$result" ]; then
+    echo "Database does not exist"
+mysql -u "$user" --password="$passwd" -h "$host" -e "CREATE DATABASE smsbro"
+mysql -u "$user" --password="$passwd" -h "$host" smsbro < "$fullpath"/db/smsbro.sql
+else
+    echo "Database exist"
+mysql -u "$user" --password="$passwd" -h "$host" -e "DROP DATABASE smsbro"
+mysql -u "$user" --password="$passwd" -h "$host" -e "CREATE DATABASE smsbro"
+mysql -u "$user" --password="$passwd" -h "$host" smsbro < "$fullpath"/db/smsbro.sql
+fi
+
+
+
 
 clear
 echo "SMSBro telah berhasil dipasang. Silakan dicoba di http://localhost/$path"
