6920bb6bee6f0f7840720fbe960833546600ba6e
Herpiko Dwi Aguno <herpiko@gmail.com>
2014-03-01 15:26:51 +0800

    add htaccess

--- smsbro_install.sh
+++ smsbro_install.sh
@@ -18,29 +18,27 @@ pwd=${pwd}
 clear
 cd /tmp/
 rm -rf master*
-url='https://github.com/herpiko/smsbro/archive/master.tar.gz'
+url='http://localhost/download/master.tar.gz'
 echo -n "Sedang mengunduh. Mohon menunggu...    "
 echo -n "    "
     wget --progress=dot $url 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
     echo -ne "\b\b\b\b"
     echo " Selesai."
 echo ""
-echo -n "Tekan tombol enter untuk melanjutkan..."
-read continue
 tar -xvf master.tar.gz
 clear
 echo " "
 echo "Tuliskan lokasi root directory dari web server di sistem anda."
 echo -n "(Jika dikosongkan, maka akan diset ke /var/www/htdocs) : "
 read rootpath
-if [[ $rootpath=="" ]]; then
+if [[ -z "$rootpath" ]]; then
 	rootpath="/var/www/htdocs"
 fi
 echo ""
 echo "Tuliskan lokasi di bawah $rootpath, dimana SMSBro ingin dipasang."
 echo -n "(Jika dikosongkan, maka SMSBro akan terpasang di root directory) : "
 read path
-if [[ $path=="" ]]; then
+if [[ -z "$path" ]]; then
 	fullpath="$rootpath"
 fi
 fullpath="$rootpath/$path"
@@ -63,17 +61,17 @@ echo "Anda perlu mengisi informasi di bawah ini untuk database gammu-smsd (smsd)
 echo "Anda boleh tidak mengisinya (kosong) jika smsd terletak pada MySQL server yang sama dengan database SMSBro"
 echo -n "SMSD's MySQL host : "
 read smsdhost
-if [[ $smsdhost=="" ]]; then
+if [[ -z "$smsdhost" ]]; then
 	smsdhost="$host"
 fi
 echo -n "SMSD's MySQL user : "
 read smsduser
-if [[ $smsduser=="" ]]; then
+if [[ -z "$smsduser" ]]; then
 	smsduser="$user"
 fi
 echo -n "SMSD's MySQL password : "
 read smsdpasswd
-if [[ $smsdpasswd=="" ]]; then
+if [[ -z "$smsdpasswd" ]]; then
 	smsdpasswd="$passwd"
 fi
 
@@ -89,6 +87,10 @@ cd $fullpath
 sed -i -e 's/php\/smsbro/'$path'/g' application/config/config.php
 mv htaccess .htaccess
 sed -i -e 's/php\/smsbro/'$path'/g' .htaccess
+mkdir -p /opt/smsbro
+cp -vR $fullpath/opt-bin /opt/smsbro/
+
+mysql -u root --password="$pass" -h $host smsbro < $fullpath/db/smsbro.sql
 
 clear
 echo "SMSBro telah berhasil dipasang. Silakan dicoba di http://localhost/$path"
